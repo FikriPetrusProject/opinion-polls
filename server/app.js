7c0d58e1e7c43461b561a65ac6680e7d552f15e5
petrusanthonychaim <petrus.anthony.surianto@gmail.com>
2025-07-02 13:21:10 +0700

    FEAT:Mockup Vote Card

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 require("dotenv").config()
+const cors = require("cors");
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const router = require('./routers/router');
 const errorHandler = require("./middlewares/errorHandler");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router)
