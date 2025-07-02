1f603e9f59181934c8eac2faea8f7b8f7bd17357
Alfikri <alfzein19@gmail.com>
2025-07-02 10:26:07 +0700

    FEAT :Added seeders and scripts to reset and seed db

---
+++ server/seeders/20250702023109-votes.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const votes = require("../data/votes.json");
+
+    votes.forEach((el) => {
+      el.createdAt = new Date();
+      el.updatedAt = new Date();
+    });
+    await queryInterface.bulkInsert("Votes", votes, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Votes", null, {});
+  }
+};
